4a15b73f3e97396657d703a99fa07de512337668
chiahui <ashleyhsueh33@gmail.com>
2021-12-31 16:24:36 +0800

    feat:Adding Validation & Resetting Logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,7 +8,16 @@ const AddUser = props => {
   const [enteredAge, setEnteredAge] = useState('')
   const addUserHandler = event => {
     event.preventDefault()
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return
+    }
+    //前面加入+確保value type是數字
+    if (+enteredAge < 1) {
+      return
+    }
     console.log(enteredUsername, enteredAge)
+    setEnteredUsername('')
+    setEnteredAge('')
   }
   const usernameChangeHandler = event => {
     setEnteredUsername(event.target.value)
@@ -21,9 +30,19 @@ const AddUser = props => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
